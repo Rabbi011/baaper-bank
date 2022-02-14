473f8c1e19885f0823088b48a2df6c4ab661b67d
rabbi011 <rakibil15-13011@diu.edu.bd>
2022-02-14 20:41:47 +0600

    all most done

--- js/banking.js
+++ js/banking.js
@@ -16,4 +16,23 @@ document.getElementById('deposit-button').addEventListener('click',function(){
 
     depositInput.value = '' ;
 
+});
+
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    //  console.log('something');
+    const withdrawInput = document.getElementById('withdraw-input');
+    // console.log(withdrawInput)
+    const newWithdrowAmount = withdrawInput.value;
+     console.log(newWithdrowAmount);
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = withdrawTotal.innerText;
+// console.log(previousWithdrawTotal)
+    const currentWithdrawAmount = parseFloat(previousWithdrawTotal) + parseFloat(newWithdrowAmount);
+    withdrawTotal.innerText = currentWithdrawAmount;
+    // update balance
+    const updateBalanceTotal = document.getElementById('balance-total');
+   const newUpdateBalanceTotal = updateBalanceTotal.innerText;
+   const newB = parseFloat(newUpdateBalanceTotal) - parseFloat(currentWithdrawAmount);
+   updateBalanceTotal.innerText = newB;
+    withdrawInput.value = '';
 })
